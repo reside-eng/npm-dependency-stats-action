44d6549371a0ca9ec3ff839d065b5d4d8244ad6f
Scott Prue <sprue.dev@gmail.com>
2021-03-09 15:54:25 -0500

    chore: cleanup lint

--- src/yarnOutdated.test.ts
+++ src/yarnOutdated.test.ts
@@ -1,17 +1,16 @@
 import * as exec from '@actions/exec';
 import yarnOutdated from './yarnOutdated';
-
+/* eslint-disable no-useless-escape */
 const outdatedOutput = `{"type":"info","data":"Color legend : \n \"<red>\"    : Major Update backward-incompatible updates \n \"<yellow>\" : Minor Update backward-compatible features \n \"<green>\"  : Patch Update backward-compatible bug fixes"}
-{"type":"table","data":{"head":["Package","Current","Wanted","Latest","Package Type","URL"],"body":[["@types/node","14.14.32","14.14.33","14.14.33","devDependencies","https://github.com/DefinitelyTyped/DefinitelyTyped.git"]]}}`
+{"type":"table","data":{"head":["Package","Current","Wanted","Latest","Package Type","URL"],"body":[["@types/node","14.14.32","14.14.33","14.14.33","devDependencies","https://github.com/DefinitelyTyped/DefinitelyTyped.git"]]}}`;
+/* eslint-enable no-useless-escape */
 
 jest.mock('@actions/exec', () => ({
   exec: (cmd: string, args: string[], options: exec.ExecOptions) => {
-    options?.listeners?.stdout?.(Buffer.from(outdatedOutput, 'utf-8'))
-    return Promise.resolve(0)
-  }
+    options?.listeners?.stdout?.(Buffer.from(outdatedOutput, 'utf-8'));
+    return Promise.resolve(0);
+  },
 }));
-
-const mockExec = exec as jest.Mocked<typeof exec>;
 
 describe('yarnOutdated', () => {
   beforeEach(() => {
@@ -20,8 +19,8 @@ describe('yarnOutdated', () => {
 
   it('should run with default action inputs', async () => {
     const result = await yarnOutdated('./test');
-    expect(result).toBeInstanceOf(Array)
-    expect(result[0]).toBeInstanceOf(Array)
-    expect(result[0][0]).toBe('@types/node')
+    expect(result).toBeInstanceOf(Array);
+    expect(result[0]).toBeInstanceOf(Array);
+    expect(result[0][0]).toBe('@types/node');
   });
 });
